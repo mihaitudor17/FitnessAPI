d3cea091048248cb58d367c5d11cd446d7ea6193
Ramona <79527794+anomaR15@users.noreply.github.com>
2023-05-13 20:15:12 +0300

    Update WorkoutType.cs

---
+++ FitnessAPI/FitnessAPI/Enums/WorkoutType.cs
@@ -0,0 +1,22 @@
+﻿namespace FitnessAPI.Enums
+{
+    public enum WorkoutType
+    {
+        Aerobics = 455,
+        Swimming = 700,
+        Running = 850,
+        Kickboxing = 900,
+        Basketball = 800,
+        Soccer = 600,
+        Tennis = 500,
+        JumpRope = 900,
+        Spinning = 600,
+        Zumba = 550,
+        Yoga = 350,
+        AerobicDancing = 350,
+        Walking = 300,
+        Pilates = 350,
+        Boxing = 650,
+        Jogging = 325,
+    }
+}
